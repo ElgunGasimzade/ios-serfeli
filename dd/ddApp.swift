bb2e47c33f13f3fe0c5ebb75ee6523e480fada2a
QasimzadaEA <123947841+ElgunGasimzade@users.noreply.github.com>
2026-01-16 14:14:53 +0400

    Initial Commit

---
+++ dd/ddApp.swift
@@ -0,0 +1,17 @@
+//
+//  ddApp.swift
+//  dd
+//
+//  Created by Elgun Qasimzade on 16.01.26.
+//
+
+import SwiftUI
+
+@main
+struct ddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
